ceb1089d20d6a264c5ead70edd3249c72830197a
whoiishelen <104018013+whoiishelen@users.noreply.github.com>
2022-04-25 17:19:46 +0200

    Add files via upload

---
+++ jerarquiaherencia/src/jerarquiaherencia/Person.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jerarquiaherencia;
+
+/**
+ *
+ * @author alumnot
+ */
+public class Person {
+    
+}
